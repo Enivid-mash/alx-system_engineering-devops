7368d8954cb7252749e20642a6eea9c8b4bb90ea
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-07-04 17:39:26 -0700

    Update 6-phone_number.rb

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # Ruby script using Oniguruma for regular expression matching
 
-# Print the matches of the pattern '^h.n$' in the provided argument
-matches = ARGV[0].scan(/^h.n$/)
+# Print the matches of the pattern '^\d{10,10}$' in the provided argument
+matches = ARGV[0].scan(/^\d{10,10}$/)
 puts matches.join
