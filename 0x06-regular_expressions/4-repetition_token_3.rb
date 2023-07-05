53977b7f9ac20d7f9b651c5f41976221aabcec84
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-07-04 17:31:15 -0700

    Add 4-repetition_token_3.rb

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# Ruby script using Oniguruma for regular expression matching
+
+# Print the matches of the pattern 'hbt*n' in the provided argument
+matches = ARGV[0].scan(/hbt*n/)
+puts matches.join
