5394165114ecd5b6edb973857d1a9c1cdbd17008
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-04-20 02:56:58 +0100

    Amend project task 1 - 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 # Install flask using pip3
 
 package { 'flask':
-  ensure   => '2.1.1',
+  ensure   => '2.1.0',
   provider => 'pip3',
+	require => Package['python'],
+}
+
+package { 'werkzeug':
+	ensure => '2.1.1',
+	provider => 'pip3'
+	require => Package['flask'],
 }
