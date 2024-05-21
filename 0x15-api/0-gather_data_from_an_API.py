c9ddaee26ab9c597ea76061cdf8ebd77961e4a42
Mawunyo on wsl2 <dmashigbi@gmail.com>
2024-05-21 06:50:21 +0100

    Amend 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Accessing a REST API for employee to-do lists"""
+"""access a REST API for employee to-do lists"""
 
 import requests
 import sys
@@ -7,13 +7,13 @@ import sys
 
 if __name__ == '__main__':
     employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com/"
-    url = f"{base_url}/{employee_id}"
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    url = base_url + str(employee_id)
 
     response = requests.get(url)
     employee_name = response.json().get('name')
 
-    todo_url = f"{url}/todos"
+    todo_url = url + "/todos"
     response = requests.get(todo_url)
     tasks = response.json()
     completed = 0
@@ -28,4 +28,4 @@ if __name__ == '__main__':
           % (employee_name, completed, len(tasks)))
 
     for task in completed_tasks:
-        print(f"\t {task.get('title')}")
+        print("\t %s" % task.get('title'))
